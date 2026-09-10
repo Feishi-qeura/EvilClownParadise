11a5eb355e079c2b9f0542e73c28a0ef91c3cb83
ZaneYooo <3203232052@qq.com>
2026-09-10 18:05:42 +0800

    111

---
+++ Source/EvilClownParadise/Public/AI/ECPStateTreeCommon.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "AIController.h"
+#include "StateTreeExecutionContext.h"
+#include "Characters/ECPMonsterBase.h"
+
+namespace ECPTree
+{
+	inline AECPMonsterBase* GetMonster(const FStateTreeExecutionContext& Context)
+	{
+		if (const AAIController* AIController = Cast<AAIController>(Context.GetOwner()))
+		{
+			return AIController ->GetPawn<AECPMonsterBase>();
+		}
+		return nullptr;
+	}
+}
