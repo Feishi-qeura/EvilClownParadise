36a4744251a00afeba9a2567024dc4d538c41ffc
ZaneYooo <3203232052@qq.com>
2026-09-15 10:47:39 +0800

    v1.1 Enhanced 3 ------------------ 1.将ECPTaskChase头文件从Private目录移至Public目录，符合项目头文件目录约定。 2.同步修正ECPTaskChase.cpp的include路径。 3.更新测试关卡TestLevel。

--- Source/EvilClownParadise/Private/AI/ECPTaskChase.cpp
+++ Source/EvilClownParadise/Private/AI/ECPTaskChase.cpp
@@ -1,8 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "ECPTaskChase.h"
-
+#include "AI/ECPTaskChase.h"
 #include "AI/ECPStateTreeCommon.h"
 #include "Characters/ECPMonsterBase.h"
 #include "Data/ECPAIController.h"
